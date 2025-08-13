bd3dca68be76ef74b9c09e65fe8e8b266895469a
sebastienmorneau <morneausebastien@gmail.com>
2025-08-12 21:13:51 -0400

    start back

--- SportApp/src/utils/polyfills.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Polyfills for React Native
- * Fixes common JavaScript environment issues
- */
-
-// React Native already provides the global object
-// No polyfills needed for basic environment setup
-
-export {};
